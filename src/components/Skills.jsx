e1ee0aca3140ea58ef40d394d3bef6ec8f720ef9
Akshay Ramesh <akshayrameshbabu@gmail.com>
2020-01-12 19:36:54 +0530

    Added skills components

---
+++ src/components/Skills.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Skills = () => {
+	return (
+		<div className="section skills">
+			<div className="section-header">🤹🏻 Skills</div>
+			<div>
+				Test
+			</div>
+		</div>
+	);
+}
+ 
+export default Skills;
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,7 +1,7 @@
 import React from "react"
-
 import Layout from "../components/Layout.jsx"
 import Banner from "../components/Banner.jsx"
+// import Skills from "../components/Skills.jsx"
 import WorkExperience from '../components/WorkExperience.jsx'
 import Education from "../components/Education.jsx"
 import Clients from "../components/Clients.jsx"
@@ -12,6 +12,7 @@ const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
     <Banner />
+    {/* <Skills /> */}
     <WorkExperience />
     <Education />
     <Clients />
